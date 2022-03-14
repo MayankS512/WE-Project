d0ba52033408cf0170020b142bec55878e74496c
Mayank Singh <DemonOverlord@users.noreply.github.com>
2022-03-14 07:13:07 +0530

    Update Box.js

--- three/Box.js
+++ three/Box.js
@@ -1,4 +1,5 @@
 import vertexShader from "./shaders/vertex.glsl"
+import particleShader from "./shaders/vertexParticles.glsl"
 import fragmentShader from "./shaders/fragment.glsl"
 import { shaderMaterial } from "@react-three/drei";
 import { extend } from "@react-three/fiber";
