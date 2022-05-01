ec063dfd3ab9ee53403be85fba30d2f98785f1db
Rajat Guptat <rajatgupta.tech@gmail.com>
2022-05-01 21:25:20 +0530

    login page

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,20 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import gsap from "gsap";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Curve from "../components/Curve";
 import Link from "next/link";
 
 const Login = () => {
   const goback = useRef()
   const router = useRouter()
+  const [Signup, setSignup] = useState(false);
+  const [fullName, setfullName] = useState("");
+  const [userName, setuserName] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [confirm, setconfirm] = useState("");
+
 
   useEffect(() => {
     gsap.to(goback.current, {
@@ -26,16 +33,27 @@ const Login = () => {
       <Curve />
 
       <div className="absolute flex flex-row items-center top-5 left-10">
-        <button ref={goback} onClick={() => {router.back()}} className="w-16 h-16 font-serif transition-colors duration-300 scale-0 bg-gray-800 rounded-full shadow-lg hover:bg-gray-700 shadow-gray-900 focus:outline-offset-4 focus:outline-gray-700"> 
-        &larr;
-        </button>
-        <h1 className="ml-10 text-4xl lg:text-8xl font-Cinzel"><Link href='/home'>Rudiment.</Link></h1>
+
+      <button ref={goback} onClick={() => {router.back()}} className="w-16 h-16 font-serif transition-colors duration-300 scale-0 bg-gray-800 rounded-full shadow-lg hover:bg-gray-700 shadow-gray-900 focus:outline-offset-4 focus:outline-gray-700"> 
+      &larr;
+      </button>
+      <h1 className="ml-10 text-4xl lg:text-8xl font-Cinzel"><Link href='/home'>Rudiment.</Link></h1>
       </div>
 
       <div className="absolute flex flex-col ml-32 text-center rounded-md shadow-lg right-80 w-96 dark:bg-neutral-700 bg-zinc-100 shadow-black">
-        <input type="text" className="px-3 py-2 mx-10 mt-8 text-lg text-black rounded-sm focus:outline-offset-4 focus:outline-gray-800 dark:focus:outline-gray-50 " placeholder="Username" />
-        <input type="password" className="px-3 py-2 m-10 text-lg text-black rounded-sm focus:outline-offset-4 focus:outline-gray-800 dark:focus:outline-gray-50" placeholder="Password" />
-        <button className="py-2 mx-10 mb-8 text-xl transition-all duration-300 bg-gray-800 rounded shadow-sm hover:bg-gray-700 focus:outline-0 focus:bg-gray-700">Login</button>
+        {Signup && 
+        <input value={fullName} onChange={(e) => {setfullName(e.target.value)}} type="text" className="px-3 py-2 mx-10 mt-8 text-lg text-black rounded-sm focus:outline-offset-4 focus:outline-gray-800 dark:focus:outline-gray-50 " placeholder="Full Name" />
+        }
+        <input value={userName} onChange={(e) => {setuserName(e.target.value)}} type="text" className="px-3 py-2 mx-10 mt-8 text-lg text-black rounded-sm focus:outline-offset-4 focus:outline-gray-800 dark:focus:outline-gray-50 " placeholder="Username" />
+        {Signup && 
+        <input value={email} onChange={(e) => {setemail(e.target.value)}} type="text" className="px-3 py-2 mx-10 mt-8 text-lg text-black rounded-sm focus:outline-offset-4 focus:outline-gray-800 dark:focus:outline-gray-50 " placeholder="Email" />
+        }
+        {Signup && 
+        <input value={password} onChange={(e) => {setpassword(e.target.value)}} type="password" className="px-3 py-2 mx-10 mt-8 text-lg text-black rounded-sm focus:outline-offset-4 focus:outline-gray-800 dark:focus:outline-gray-50 " placeholder="Password" />
+        }
+        <input value={confirm} onChange={(e) => {setconfirm(e.target.value)}} type="password" className="px-3 py-2 m-10 text-lg text-black rounded-sm focus:outline-offset-4 focus:outline-gray-800 dark:focus:outline-gray-50" placeholder="Confirm Password" />
+        <button className="py-2 mx-10 mb-2 text-xl transition-all duration-300 bg-gray-800 rounded shadow-sm hover:bg-gray-700 focus:outline-0 focus:bg-gray-700">{Signup ? "Signup" : "Login"}</button>
+        <div onClick={() => {setSignup(!Signup)}} className="mb-8"> {Signup ? "Already have an account?" : "Don't have an account?"} <span className="text-blue-600 cursor-pointer">{Signup ? "Login" : "Register"}</span></div>
         <button className="py-2 mx-10 mb-8 text-xl transition-all duration-300 bg-gray-800 rounded shadow-sm hover:bg-gray-700 focus:outline-0 focus:bg-gray-700">Register</button>
       </div>
     
